e622d9e2141c3147375e56bf3e7f2743aec1213a
Andrey <and.vladislavov@gmail.com>
2020-09-16 17:56:09 +0300

    adapter fix

--- app/src/main/java/ru/vezdekod/podcast/ui/audioedit/TimecodeAdapter.kt
+++ app/src/main/java/ru/vezdekod/podcast/ui/audioedit/TimecodeAdapter.kt
@@ -1,4 +1,24 @@
 package ru.vezdekod.podcast.ui.audioedit
 
-class TimecodeAadpter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.vezdekod.podcast.R
+import ru.vezdekod.podcast.ui.data.TimecodeDataSource
+
+class TimecodeAdapter:RecyclerView.Adapter<TimecodeElementHolder>() {
+    val source = TimecodeDataSource()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimecodeElementHolder
+     = TimecodeElementHolder(LayoutInflater
+        .from(parent.context)
+        .inflate(R.layout.timecode_element, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: TimecodeElementHolder, position: Int) {
+        
+    }
+
+    override fun getItemCount(): Int = source.timecodes.size
+
 }
