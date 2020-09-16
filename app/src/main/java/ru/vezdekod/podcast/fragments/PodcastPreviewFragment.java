24c4797a252ae5ed2adfd7107249e6cb44de62d4
Nik Shlykov <nikshlykovdev@gmail.com>
2020-09-16 20:38:15 +0300

    add basic navigation and some forgotten fragments

--- app/src/main/java/ru/vezdekod/podcast/fragments/PodcastPreviewFragment.java
+++ app/src/main/java/ru/vezdekod/podcast/fragments/PodcastPreviewFragment.java
@@ -14,7 +14,7 @@ import androidx.navigation.NavDirections;
 import ru.vezdekod.podcast.OnFragmentInteractionListener;
 import ru.vezdekod.podcast.R;
 
-public class MainPodcastDataFragment extends Fragment {
+public class PodcastPreviewFragment extends Fragment {
 
     private OnFragmentInteractionListener onFragmentInteractionListener;
 
@@ -27,16 +27,16 @@ public class MainPodcastDataFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_podcast_data, container, false);
+        return inflater.inflate(R.layout.fragment_podcast_preview, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.fragment_main_podcast_data___button___next).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.fragment_podcast_preview___button___publish_podcast).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NavDirections navDirections = MainPodcastDataFragmentDirections.actionNavMainPodcastDataToNavAudioEditing();
+                NavDirections navDirections = PodcastPreviewFragmentDirections.actionNavPodcastPreviewToNavEnd();
                 onFragmentInteractionListener.onFragmentInteraction(navDirections);
             }
         });
