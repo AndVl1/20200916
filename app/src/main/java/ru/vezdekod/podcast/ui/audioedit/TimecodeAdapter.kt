73d84d4cde2578716624bbe844f59f4622b01306
Andrey <and.vladislavov@gmail.com>
2020-09-16 22:01:26 +0300

    timecodes are saving && slider working

--- app/src/main/java/ru/vezdekod/podcast/ui/audioedit/TimecodeAdapter.kt
+++ app/src/main/java/ru/vezdekod/podcast/ui/audioedit/TimecodeAdapter.kt
@@ -1,7 +1,9 @@
 package ru.vezdekod.podcast.ui.audioedit
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import ru.vezdekod.podcast.R
 import ru.vezdekod.podcast.ui.data.TimecodeDataSource
@@ -12,13 +14,26 @@ class TimecodeAdapter (val source: TimecodeDataSource) : RecyclerView.Adapter<Ti
      = TimecodeElementHolder(
         LayoutInflater
             .from(parent.context)
-            .inflate(R.layout.timecode_element, parent, false)
+            .inflate(R.layout.timecode_element, parent, false),
+        this
     )
 
     override fun onBindViewHolder(holder: TimecodeElementHolder, position: Int) {
-
+        holder.bind(source.timecodes[position].name, source.timecodes[position].time)
     }
 
     override fun getItemCount(): Int = source.timecodes.size
 
+    fun removeItem(position: Int){
+        source.timecodes.removeAt(position)
+        notifyDataSetChanged()
+    }
+
+    fun updateName(position: Int, text: String) {
+        source[position].name = text
+    }
+
+    fun updateTime(position: Int, time: String) {
+        source[position].time = time
+    }
 }
