d2a7597e18d3fb6c233cf5701a8373c94fb4482b
Nik Shlykov <nikshlykovdev@gmail.com>
2020-09-16 13:48:49 +0300

    add simple fragments classes and simple layouts

---
+++ app/src/main/java/ru/vezdekod/podcast/fragments/MainPodcastDataFragment.java
@@ -0,0 +1,21 @@
+package ru.vezdekod.podcast.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ru.vezdekod.podcast.R;
+
+public class MainPodcastDataFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_podcast_data, container, false);
+    }
+}
