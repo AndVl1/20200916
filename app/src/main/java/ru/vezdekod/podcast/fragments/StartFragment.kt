0a9a1653946e2d3c392e3a39145c5350ae6b096a
ARTI1208 <leonardo906@mail.ru>
2020-09-16 22:02:41 +0300

    First page done

--- app/src/main/java/ru/vezdekod/podcast/fragments/StartFragment.kt
+++ app/src/main/java/ru/vezdekod/podcast/fragments/StartFragment.kt
@@ -1,44 +1,45 @@
-package ru.vezdekod.podcast.fragments;
+package ru.vezdekod.podcast.fragments
 
-import android.content.Context;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import ru.vezdekod.podcast.OnFragmentInteractionListener
+import ru.vezdekod.podcast.R
+import ru.vezdekod.podcast.databinding.FragmentStartBinding
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavDirections;
+class StartFragment : Fragment() {
 
-import ru.vezdekod.podcast.OnFragmentInteractionListener;
-import ru.vezdekod.podcast.R;
+    private lateinit var viewBinding: FragmentStartBinding
+    private var onFragmentInteractionListener: OnFragmentInteractionListener? = null
 
-public class StartFragment extends Fragment {
-
-    private OnFragmentInteractionListener onFragmentInteractionListener;
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        onFragmentInteractionListener = (OnFragmentInteractionListener) context;
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        onFragmentInteractionListener = context as OnFragmentInteractionListener
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_start, container, false);
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentStartBinding.inflate(inflater, container, false).also {
+        viewBinding = it
+    }.root
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.fragment_start___button___create_podcast).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NavDirections navDirections = StartFragmentDirections.actionNavFirstToNavMainPodcastData();
-                onFragmentInteractionListener.onFragmentInteraction(navDirections);
-            }
-        });
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.closeMessageButton.setOnClickListener {
+            Toast.makeText(context, "Заглушка", Toast.LENGTH_SHORT).show()
+        }
+        viewBinding.createPodcastButton.setOnClickListener {
+            val navDirections: NavDirections =
+                StartFragmentDirections.actionNavFirstToNavMainPodcastData()
+            onFragmentInteractionListener?.onFragmentInteraction(navDirections)
+        }
     }
 }
